9a95bb4319e811bfb250e9851835ad187cef69a4
Jayanth Nalluri <76648322+jayanth353@users.noreply.github.com>
2021-05-04 17:15:30 +0530

    Update Creation of SLL.c

--- Linked List/Single Linked List/Creation of SLL.c
+++ Linked List/Single Linked List/Creation of SLL.c
@@ -2,14 +2,15 @@
 1.Create a class Node which has two attributes: data and next. Next is a pointer to the next node.
 2.Create another class which has two attributes: head and tail.
 3.addNode() will add a new node to the list:
-a.Create a new node.
-b.It first checks, whether the head is equal to null which means the list is empty.
-c.If the list is empty, both head and tail will point to the newly added node.
-d.If the list is not empty, the new node will be added to end of the list such that tail's next will point to the newly added node. This new node will become the new tail of the list.
+    a.Create a new node.
+    b.It first checks, whether the head is equal to null which means the list is empty.
+    c.If the list is empty, both head and tail will point to the newly added node.
+    d.If the list is not empty, the new node will be added to end of the list such that tail's next will point to the newly added node. This new node will become the new tail 
+    of the list.    a.Define a node current which initially points to the head of the list.
+    b.Traverse through the list till current points to null.
+    c.Display each node by making current to point to node next to it in each iteration.*/
+
 4.display() will display the nodes present in the list:
-a.Define a node current which initially points to the head of the list.
-b.Traverse through the list till current points to null.
-c.Display each node by making current to point to node next to it in each iteration.*/
 #include<stdio.h>
 #include<stdlib.h>
 struct node { 
